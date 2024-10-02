52571e4252f2cd6ab920ec808243d6e1cd809d33
Jae Moon <vicbizmoon@hotmail.com>
2024-10-02 19:41:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 
 st.title('🤖 Machine Learning App')
-
 st.write('This is app builds a machine learning model!')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
