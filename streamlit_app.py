8f7b4c786b23ee2eaef174ea41651ab9d681ca37
Jae Moon <vicbizmoon@hotmail.com>
2024-10-02 20:16:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,9 @@ with st.expander('Data'):
 with st.expander('Data Visualization'):
   # "bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+with st.sidebar:
+  # "island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen')
+  gender = st.selectbox('Gender', ('male', 'female')
+  bill_depth_mm = st.slider('Bill length (mm)', 32.1 59.6, 43.9)
