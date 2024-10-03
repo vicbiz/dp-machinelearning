de3690eea27483cb8c02477d9556323a9fad7c10
Jae Moon <vicbizmoon@hotmail.com>
2024-10-02 21:28:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with st.sidebar:
           'bill_depth_mm', bill_depth_mm,
           'flipper_length_mm', flipper_length_mm,
           'body_mass_g', body_mass_g,
-          'gender', sex}
+          'gender', gender}
   input_df = pd.DataFrame(data, index=[0])
   input_df
 
